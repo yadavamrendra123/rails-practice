784a58e8e274424ba6eda391727d77611d8700bf
amrendra <amrendra.bei@gmail.com>
2023-07-05 15:53:13 +0545

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html:"Hey amrendra this is your first rails program"
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    # render html:"hey you are in the home page"
+  end
+end
