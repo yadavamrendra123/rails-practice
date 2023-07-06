b57e5d91b4443c666035982090ddaacdab79a335
amrendra <amrendra.bei@gmail.com>
2023-07-06 17:59:43 +0545

    feat : add who created the article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.email =email.downcase}
   has_many :articles
 
   validates :username, presence: true, length: {minimum:3,maximum:25},uniqueness: {case_sensitive: false}
